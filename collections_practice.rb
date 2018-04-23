3763beb9f36cb09cba9e2a31c0ecb48d6a8a0787
blevm <mjblevins7@gmail.com>
2018-04-23 19:24:23 -0400

    solution

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
+require 'pry'
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort {|x,y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+   array.insert(1,array.delete_at(2))
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.map do |item|
+    item.insert(2, "$")
+    chars = item.chars
+    chars.delete_at(3)
+    chars.join
+  end
+end
+
+def find_a(array)
+  array.select do |item|
+    item.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.reduce(:+)
+end
+
+def add_s(array)
+  array.map do |item|
+    unless item == array[1]
+      item.insert(item.length, "s")
+    else
+      item
+    end
+  end
+end
+
+#pry.start
